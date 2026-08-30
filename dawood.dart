a5dee579ae72024fb9f2cc5ab05441fe6d044cf6
DawoodGMustafa <dawoodgmustafa09@gmail.com>
2026-08-30 06:51:14 -0700

    Question NO:9

--- dawood.dart
+++ dawood.dart
@@ -1,8 +1,23 @@
 void main () {
-print ("Question NO:8");
-print ("Name List");
+print ("Question NO:9");
+print ("Empty  List");
 
-List Names =["Dawood,Ahmed,Hasan"];
+List Names =[];
+
+Names.add ("Monday");
+
+Names.add ("Tuesday");
+
+Names.add ("Wednesday");
+
+Names.add ("Thursday");
+
+Names.add ("Friday");
+
+Names.add ("Saturday");
+
+Names.add ("Sunday");
+
 print (Names);
 
 }
